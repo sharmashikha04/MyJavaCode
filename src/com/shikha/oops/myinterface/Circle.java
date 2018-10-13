b608d329399a485ecb6b8647c642c5de0fefdf90
sharmashikha04 <sharmashikha9310@gmail.com>
2018-10-13 23:51:48 +0530

    added new method perimeter

--- src/com/shikha/oops/myinterface/Circle.java
+++ src/com/shikha/oops/myinterface/Circle.java
@@ -4,5 +4,7 @@ public interface Circle {
 
     double pi=3.14;
 
-    double areaOfCircle(double radius);
+
+   // double areaOfCircle(double radius);
+    double perimeterOfCircle(double radius);
 }
